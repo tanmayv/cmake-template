a62f749eae792bb9b1a01ab5aad2ce61b2e16f08
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2023-05-06 23:52:15 +0530

    Cmake template for library

---
+++ library/test/multiply_test.cc
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <multiply.h>
+
+TEST(MultiplyTests, TestIntegerOne_One) {
+  const auto expected = 1;
+  const auto actual = multiply(1, 1);
+  ASSERT_EQ(expected, actual);
+}
+
+TEST(MultiplyTests, TestIntegerZero_Zero) {
+  const auto expected = 0;
+  const auto actual = multiply(0, 0);
+  ASSERT_EQ(expected, actual);
+}
+
+TEST(MultiplyTests, TestIntegerZero_One) {
+  const auto expected = 0;
+  const auto actual = multiply(0, 1);
+  ASSERT_EQ(actual, expected);
+}
+
